9a282cbc0decf55a54745e1cb176b2c107ff4b6a
PhamPhong137 <pp072003@gmail.com>
2025-02-16 13:20:00 +0700

    Fix: cors

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "*",
+    origin: process.env.CORS_ORIGIN || "https://heytext-fe.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
